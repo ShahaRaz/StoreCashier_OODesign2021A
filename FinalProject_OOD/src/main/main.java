b9e28aef200df6c483810e892d6b9359ec2a0cb8
Gadi <62388856+Gadiler@users.noreply.github.com>
2021-01-23 13:38:57 +0200

    Added JavaFx library.

--- FinalProject_OOD/src/main/main.java
+++ FinalProject_OOD/src/main/main.java
@@ -1,10 +1,14 @@
 package main;
 
+/*
+ * @author Gadi Engelsman.
+ * @author Shachar Raz
+ * */
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Test 1");
 
 	}
-
 }
